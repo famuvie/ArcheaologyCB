079383a53a71221c00d3057284be202f1d7cf65b
Facundo Muñoz <facundo.munoz@orleans.inra.fr>
2015-08-08 22:19:32 +0200

    Casa de las Aguilas: visualization and Euclidean kriging

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,17 @@
-# Example preprocessing script.
+## Extract observation data from SpatialPointsDataFrame
+obs <- cbind(coordinates(shp.quimicos),
+             slot(shp.quimicos, 'data'))
+names(obs) <- c('x', 'y', 'z', 'pc')
+
+## the third coordinate and the data value are the same thing
+obs <- obs[, -3]
+
+## Remove duplicated measurements
+obs <- obs[!duplicated(obs[, c('x', 'y')]), ]
+
+
+## Coordinates of the border 
+border_coord <- with(fortify(shp.contorno),
+                     data.frame(x = long,
+                                y = lat))
+# ggplot(border_coord, aes(x, y)) + geom_path() + coord_equal()
