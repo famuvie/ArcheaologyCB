92cd8623decfd92b7845cda4dc2be5903fb6bb44
Facundo Muñoz <facundo.munoz@orleans.inra.fr>
2015-08-25 17:09:23 +0200

    Munge: remove redundant third coordinate in quimicos

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,10 +1,11 @@
+## the third coordinate and the data value are the same thing
+shp.quimicos@coords <- shp.quimicos@coords[, -3]
+shp.quimicos@bbox <- shp.quimicos@bbox[-3, ]
+
 ## Extract observation data from SpatialPointsDataFrame
 obs <- cbind(coordinates(shp.quimicos),
              slot(shp.quimicos, 'data'))
-names(obs) <- c('x', 'y', 'z', 'pc')
-
-## the third coordinate and the data value are the same thing
-obs <- obs[, -3]
+names(obs) <- c('x', 'y', 'pc')
 
 ## Remove duplicated measurements
 obs <- obs[!duplicated(obs[, c('x', 'y')]), ]
